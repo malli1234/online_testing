d2dec86b0aba596cf621e7e2e9fb8e1a84aac3bd
malli1234 <vadlamudimalli@gmail.com>
2019-08-17 22:58:41 -0700

    first commit

---
+++ src/main/java/com/wells/presentation/SampleServlet.java
@@ -0,0 +1,26 @@
+package com.wells.presentation;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SampleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+  
+    public SampleServlet() {
+
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String yourName = request.getParameter("yourName");
+		PrintWriter writer = response.getWriter();
+		writer.println("<h1>Hello " + yourName + "</h1>");
+		writer.close();
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<h1>Hello JSP and Servlet!</h1>
+<form action="SampleServlet" method="post">
+    Enter your name: <input type="text" name="yourName" size="20">
+    <input type="submit" value="Call Servlet" />
+</form>
